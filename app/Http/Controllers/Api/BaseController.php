44b516197d6b27047a061cebecfa76547615db7d
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2022-04-01 11:12:40 +0700

    Update

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -11,29 +11,29 @@ class BaseController extends Controller
   public function sendResponse($result, $message)
   {
     $response = [
-          'success' => true,
-          'data'    => $result,
-          'message' => $message,
-      ];
+      'success' => true,
+      'data'    => $result,
+      'message' => $message,
+    ];
 
 
-      return response()->json($response, 200);
+    return response()->json($response, 200);
   }
 
   public function sendError($error, $errorMessages = [], $code = 404)
-    {
-    	$response = [
-            'success' => false,
-            'message' => $error,
-        ];
+  {
+    $response = [
+      'success' => false,
+      'message' => $error,
+    ];
 
 
-        if(!empty($errorMessages)){
-            $response['data'] = $errorMessages;
-        }
-
-
-        return response()->json($response, $code);
+    if(!empty($errorMessages)){
+      $response['data'] = $errorMessages;
     }
+
+
+    return response()->json($response, $code);
+  }
 
 }
